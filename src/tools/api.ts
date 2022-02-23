de787f80a932078f18acf10b0610f973023355d5
tema2294 <tema.marinin@mail.ru>
2022-02-23 20:00:23 +0300

    add edit coins and modal window

--- src/tools/api.ts
+++ src/tools/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios"
 
  const baseUrlCoinGecko = 'https://api.coingecko.com/'
- // const baseUrlHeroku = 'https://fast-headland-89130.herokuapp.com/'
-    const baseUrlHeroku = 'http://localhost:5000/'
+ const baseUrlHeroku = 'https://fast-headland-89130.herokuapp.com/'
+    // const baseUrlHeroku = 'http://localhost:5000/'
 
 export const apiCoinGecko = axios.create({
     baseURL: baseUrlCoinGecko + `api/v3/`,
