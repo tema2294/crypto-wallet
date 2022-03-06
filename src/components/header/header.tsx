0ab1bce5882f1cd450176d5d28914d705c88fc15
tema2294 <tema.marinin@mail.ru>
2022-03-06 18:15:51 +0300

    add usd edit mode

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,12 +1,24 @@
 import {Link} from "react-router-dom";
 import logo from '../../logo.svg';
+import {useDispatch, useSelector} from "react-redux";
+import {usdPriceSelector, userSelector} from "../../store/selectors/selectors";
+import React, {useEffect, useState} from "react";
 import './style.scss'
-import {useSelector} from "react-redux";
-import {userSelector} from "../../store/selectors/selectors";
+import {TextField} from "@mui/material";
+import {walletActions} from "../../reducers/walletSlice";
 
 const Header = () => {
     const userData = useSelector(userSelector)
+    const usdPrice = useSelector(usdPriceSelector)
+    const dispatch = useDispatch()
     const username = userData?.username
+    const [isUsdEditMode,setUsdEditMode] = useState(false)
+    const [usdInput,setUsdInput] = useState(usdPrice)
+    useEffect(()=>{
+        if (!isUsdEditMode && usdPrice !== usdInput) {
+            dispatch(walletActions.setUsdPrice(usdInput))
+        }
+    },[isUsdEditMode])
     return (
         <header className='header-container'>
             <nav>
@@ -30,6 +42,11 @@ const Header = () => {
             </nav>
             {username &&
             <div className='header-user-info'>
+                {isUsdEditMode ?
+                    <TextField onDoubleClick={()=>setUsdEditMode(false)} label='usd price' type={'number'} onChange={(e:any)=>setUsdInput(e.target?.value)} value={usdInput} />
+                    : <div onDoubleClick={()=>setUsdEditMode(true)}>usd: {usdPrice}$</div>
+                }
+
                 {username}
             </div>}
         </header>
